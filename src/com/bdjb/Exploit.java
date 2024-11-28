37959e4c9c7e0ed3540213d59b07725143c98664
Andy Nguyen <theofficialflow1996@gmail.com>
2024-11-28 23:43:19 +0100

    Update copyright year.

--- src/com/bdjb/Exploit.java
+++ src/com/bdjb/Exploit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Andy Nguyen
+ * Copyright (C) 2021-2024 Andy Nguyen
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
